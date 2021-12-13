8fe624f01b156f2033bc6e6a568da6ec5989f981
VarshaDeepa <95951650+VarshaDeepa@users.noreply.github.com>
2021-12-13 12:27:04 +0530

    first commit

---
+++ src/com/ibs/pro/Main.java
@@ -0,0 +1,12 @@
+package com.ibs.pro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		String input = "hello";
+		char[] s = input.toCharArray();
+		solution.reverseString(s);
+	}
+
+}
